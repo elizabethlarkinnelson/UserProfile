05b3333b029b80706669cd87cc180ee013243741
Elizabeth Nelson <ENelson@travisusd.org>
2018-05-08 09:24:57 -0700

    drawable folder added.  setimage resource added to java file

--- app/src/main/java/com/example/enelson/userprofile/MainActivity.java
+++ app/src/main/java/com/example/enelson/userprofile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.enelson.userprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView profilePicImageView = (ImageView) findViewById(R.id.profile_picture);
+        profilePicImageView.setImageResource(R.drawable.liz);
+
         TextView nameTextView = (TextView) findViewById(R.id.name);
-        nameTextView.setText();
+        nameTextView.setText("Elizabeth");
+
+        TextView birthdayTextView = (TextView) findViewById(R.id.birthday);
+        nameTextView.setText("January");
+
+        TextView countryTextView = (TextView) findViewById(R.id.country);
+        nameTextView.setText("USA");
     }
 }
